f45e184e583b25816b67f09848d59b57f2148b24
bladupsy <gisela.cristaldo11@gmail.com>
2021-01-09 09:58:24 -0300

    Conexion DB

--- conexion.php
+++ conexion.php
@@ -13,12 +13,12 @@ try{
 
     $pdo = new PDO($link, $usuario, $password);
 
-    echo 'Conectado';
+    //echo 'Conectado';
     //query es una sentencia que se muestra por ejemplo la tabla 
 
-    foreach($pdo->query('SELECT * FROM `tablas` ') as $fila)  {
-        print_r($fila);
-    }
+    //foreach($pdo->query('SELECT * FROM `tablas` ') as $fila)  {
+      //  print_r($fila);
+    //}
 
 } catch (PDOException $e) {
     print "Error: " . $e->getMessage() . "</br>";
